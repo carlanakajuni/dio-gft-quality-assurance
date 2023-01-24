abfa4ddebed039d58c0dea0d3a02fdcb6bfdbf1c
carlanakajuni <carlanakajuni@hotmail.com>
2023-01-24 18:40:53 -0300

    Anotações de algumas palavras reservadas em Java

---
+++ ide-java/palavras-reservadas/palavras-reservadas/src/App.java
@@ -0,0 +1,57 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        /* PALAVRAS RESERVADAS: não podem ser utilizados para nomear variáveis, classes, métodos ou atributos
+            linguagem Java possui 52 palavras reservadas, classificadas como grupos e escritas com letra minúscula
+
+            - Controle de pacotes:
+                import; --> importa pacotes ou classes para dentro do código
+                package; --> especifica a que pacote todas as classes de um arquivo pertencem
+
+            - Modificadores de acesso:
+                public; --> acesso a qualquer classe
+                private; --> acesso apenas dentro da classe
+                protected; --> acesso por classes no mesmo pacote e subclasses
+
+            - Primitivos:
+                boolean; verdadeiro ou falso
+                byte; um inteiro de 8 bits
+                char; um character unicode
+                double; um número de ponto flutuante de 64 bits
+                float; um número de ponto flutuante de 32 bits
+                int; um inteiro de 32 bits
+                long; um inteiro de 64 bits
+                short; um inteiro de 32 bits
+                void; indica que o método não tem retorno de valor
+
+            - Modificadores de classes, variáveis ou métodos:
+                abstract; classe que não pode ser instanciada ou método que precisa ser implementado por uma subclasse não abstrada
+                class; especifica uma classe
+                extends; indica superclasse que a subclasse está estendendo
+                final; impossibilita que uma classe seja estendida, que um método seja sobrescrito ou que uma variável seja reinicializada
+                implements; indica as interfaces que uma classe irá implementar
+                interface; especifica uma interface
+                native; indica que um método está escrito em uma linguagem dependente de plataforma, como o C
+                new; instancia um novo objeto, chamando seu construtor
+                static; faz um método ou variável pertencer à classe ao invés de às instâncias
+                strictfp; usado em frente a um método ou classe para indicar que os numeros de ponto flutuante seguirão as regras de ponto flutuante em todas as expressões
+                synchronized; indica que um método só pode ser acessado por uma thread de cada vez
+                transient; impede a serialização de campos
+                volatile; indica que uma variável pode ser alterada durante o uso de threads
+            
+            - Controle de fluxo dentro de um bloco de código:
+                break; sai do bloco de codigo em que ele está
+                case; executa um bloco de código dependendo do teste do switch
+                continue; pula a execução do código que viria após essa linha e vai para a próxima passagem do loop
+                throw; usado para passar uma exceção para o método que o chamou
+                throws; indica que um método pode passar uma exceção para o método que o chamou
+                try; bloco de código que tentará ser executado, mas que pode causar uma exceção
+
+            - Variáveis de referência:
+                super; refere-se a superclasse imediata
+                this; refere-se a instância atual do objeto
+         * 
+         */
+
+    }
+}
