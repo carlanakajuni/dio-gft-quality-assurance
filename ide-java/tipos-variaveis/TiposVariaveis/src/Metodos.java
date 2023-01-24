2849a538a6e41c7086454a352a77733e368d0e54
carlanakajuni <carlanakajuni@hotmail.com>
2023-01-24 17:32:15 -0300

    Anotações sobre Métodos

---
+++ ide-java/tipos-variaveis/TiposVariaveis/src/Metodos.java
@@ -0,0 +1,18 @@
+public class Metodos {
+  public static void main(String[] args) {
+    
+    /* MÉTODOS: deve ser nomeado como verbo
+              : seguir padrão camelCase
+              exemplo: somar(int n1, int n2){}
+              exemplo: abrirConexao(){}
+      todos os métodos precisam ser definidos dentro de uma classe
+      Caso o método não retorne nenhum valor, ele será representado pela palavra-chave void
+    
+    * Visibilidades de métodos:
+      - pacotes
+      - herança
+      - nível a própria classe
+    */
+
+  }
+}
