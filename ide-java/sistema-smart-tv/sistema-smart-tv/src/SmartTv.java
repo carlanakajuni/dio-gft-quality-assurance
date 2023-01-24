5316cc5c835e07529bbb7d52e7b2c9fa0600387f
carlanakajuni <carlanakajuni@hotmail.com>
2023-01-24 17:53:46 -0300

    Projeto Smart Tv - entendendo os métodos

---
+++ ide-java/sistema-smart-tv/sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,41 @@
+public class SmartTv {
+  boolean ligada = false;
+  int canal = 1;
+  int volume = 25;
+
+  public void mudarCanal(int novoCanal){
+    canal = novoCanal;
+
+  }
+
+  public void aumentarCanal(){
+    canal++;
+
+  }
+
+  public void diminuirCanal(){
+    canal--;
+
+  }
+
+  public void aumentarVolume(){
+    volume++; // incremento de volume
+
+  }
+
+  public void diminuirVolume(){
+    volume--; // decremento de volume
+
+  }
+
+  public void ligar(){
+    ligada = true;
+
+  }
+
+  public void desligar(){
+    ligada = false;
+    
+  }
+
+}
